24bf4d2d3ebc5275b673ca73deeb2b82dfeec815
Alexander <alexanderelzinga94@gmail.com>
2020-06-15 22:35:42 +0200

    Updated Unity version and project packages

---
+++ Assets/Scripts/Controls/Mover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent agent = null;  // Nav mesh agent of this character
+
+    [SerializeField] private Transform target = null;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = target.position;
+    }
+}
